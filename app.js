21c493c3b75e4c028f413af396352fd21b775eb8
Adejumo Florence Boluwatife <adejumoflorenceb@gmail.com>
2023-07-07 01:04:41 +0100

    add files via upload
    stage 0ne lauvh

---
+++ app.js
@@ -0,0 +1,9 @@
+// JavaScript code for the website notifications
+function notifyNewSignUpMessage() {
+    // Code to notify you of new sign up messages
+    console.log("New sign up message received!");
+  }
+  
+  // Call the function to start receiving notifications
+  notifyNewSignUpMessage();
+
